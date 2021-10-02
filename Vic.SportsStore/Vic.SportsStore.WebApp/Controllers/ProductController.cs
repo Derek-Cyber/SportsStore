1905a364f4de75f8ae51315a2f9afcafbdb0cd72
Derek-Cyber <Derek>
2021-10-02 16:20:25 -0300

    1 [CHG] add paging;

--- Vic.SportsStore/Vic.SportsStore.WebApp/Controllers/ProductController.cs
+++ Vic.SportsStore/Vic.SportsStore.WebApp/Controllers/ProductController.cs
@@ -11,13 +11,24 @@ namespace Vic.SportsStore.WebApp.Controllers
 {
     public class ProductController : Controller
     {
-        
-        
+
+        public const int PageSize = 3;
         public IProductsRepository Repository { get; set; }  //because of the inversion (IocConfig.cs), no need =new xx() here
 
-        public ViewResult List()
+        public ViewResult List(int page=2)
         {
-            return View(Repository.Products);
+            var result = Repository
+                .Products
+                .OrderBy(p => p.ProductId)
+                .Skip((page - 1) * PageSize)  //1,2,3,   4,5,6,    7,8,9
+                .Take(PageSize);
+
+
+            return View(result);
+
+
+
+
         }
     }
 }
